729b7409c7ad0934be5928baa1e4b39d8b253359
jayson <leobest9688@gmail.com>
2022-07-09 00:50:05 +0800

    build the function to display all the colors with flex css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
-function App() {
+import { useEffect, useState } from 'react'
+
+const width = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+]
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangement.push(randomColor)
+    }
+    // console.log(randomColorArrangement)
+    setCurrentColorArrangement(randomColorArrangement)
+  }
+
+  // createBoard()
+  useEffect(() => {
+    createBoard()
+  }, [])
+
+  console.log(currentColorArrangement)
   return (
-    <div></div>
+    <div className="app" >
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            // alt={candyColor}
+          />
+        ))}
+
+      </div>
+    </div>
   )
 }
 
